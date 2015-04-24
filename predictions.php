49c3cfc21d8a31e2b07a037271557aaff86616ac
unknown <dell@dell-PC.(none)>
2015-04-24 20:43:27 +0530

    Modified1

--- predictions.php
+++ predictions.php
@@ -6,19 +6,30 @@ $where_form_is="http://".$_SERVER['SERVER_NAME'].strrev(strstr(strrev($_SERVER['
 //mail("gsingh.0101@gmail.com","Form submission", "Customer Details:
 //");
 
+function getParam($key)
+{    
+    switch (true) {
+        case isset($_GET[$key]):
+            return $_GET[$key];
+        case isset($_POST[$key]):
+            return $_POST[$key];
+        default:
+            return "";
+    }    
+}
 
 // PROCESS INPUT
-$firstname = ($_POST['firstname'] ? :"");
-$middlename = ($_POST['middlename'] ? :"");
-$lastname = ($_POST['lastname'] ? :"");
-$country = ($_POST['countryid'] ? :"");
+$firstname = getParam('firstname');
+$middlename = getParam('middlename');
+$lastname = getParam('lastname');
+$country = getParam('countryid');
 
 $dob = "";
 $mob = "";
 $yob = "";
-if ($_POST['dob'] != '')
+if ($_REQUEST['dob'] != '')
 {
-	$dtob = $_POST['dob'];
+	$dtob = $_REQUEST['dob'];
 	# assuming dd/mm/yy format
 	$dmy_arr = explode("/", $dtob);
 	$dob = $dmy_arr[0];
@@ -34,7 +45,7 @@ $conn = mysql_connect($db_host, $db_user, $db_pass);
 if(!$conn) die ('Could not connect to database: '.mysql_error());
 mysql_select_db($db_name, $conn);
 
-$query = "INSERT into `".$db_table."` (dob, mob, yob, hob,minob,city,firstname,middlename,lastname,country) VALUES ('" . $dob . "','" . $mob . "','" . $yob . "','" . $_POST['hob'] . "','" . $_POST['minob'] . "','" . $_POST['pob'] . "','" . $firstname . "','" . $middlename . "','" . $lastname . "','" . $country . "')";
+$query = "INSERT into `".$db_table."` (dob, mob, yob, hob,minob,city,firstname,middlename,lastname,country) VALUES ('" . $dob . "','" . $mob . "','" . $yob . "','" . $_REQUEST['hob'] . "','" . $_REQUEST['minob'] . "','" . $_REQUEST['pob'] . "','" . $firstname . "','" . $middlename . "','" . $lastname . "','" . $country . "')";
 
 if ($_DEBUG)
 	echo $query . "<br/>";
@@ -51,20 +62,20 @@ $fname = "customerinputlog.txt";
 $fh = fopen($fname, 'a') or die ("Can't open log file");
 fwrite($fh, "
 Prediction performed at: " . date("Y-m-d H:i:s") . "
-Date of Birth: " . $_POST['dob'] . " 
-Country List: " . $_POST['countryid'] . " 
-First Name: " . $_POST['firstname'] . " 
-Middle Name: " . $_POST['middlename'] . " 
-Surname: " . $_POST['lastname'] . " 
-Place of Birth: " . $_POST['pob'] . " 
-Time of Birth HOUR:MIN: " . $_POST['hob'] . ":" . $_POST['minob'] . " 
+Date of Birth: " . $_REQUEST['dob'] . " 
+Country List: " . $country . " 
+First Name: " . $firstname . " 
+Middle Name: " . $middlename . " 
+Surname: " . $lastname . " 
+Place of Birth: " . $_REQUEST['pob'] . " 
+Time of Birth HOUR:MIN: " . $_REQUEST['hob'] . ":" . $_REQUEST['minob'] . " 
 
 ");
 
 
 // CALL EXEC
 // change arguments here ---
-$predict_cmd = "pred2.exe" . " " . $dob . " " . $mob . " " . $yob . " " . $_POST['hob'] . " " . $_POST['minob'] . " " . $_POST['pob'];
+$predict_cmd = "pred2.exe" . " " . $dob . " " . $mob . " " . $yob . " " . $_REQUEST['hob'] . " " . $_REQUEST['minob'] . " " . $_REQUEST['pob'];
 $out_arr = Array();
 $ret_arr = Array();
 //$exec_ret = exec("date /T", $out_arr, $ret_arr);
@@ -81,6 +92,21 @@ if ($_DEBUG)
 $command_print = "<br/> Command Executed: <br/>" . $predict_cmd . " <br/><br/>" . "Command Output: <br/>";
 $predictions = implode("<br/>", $out_arr);
 
-include("onedone.php");
+// Regular output
+//include("onedone.php");
+
+// For Webservice
+$ret_arr = array("Retcode" => 0, 
+"Date" => $_REQUEST['dob'],
+"Hour" => $_REQUEST['hob'],
+"Minutes" => $_REQUEST['minob'],
+"City" => $_REQUEST['pob'],
+"Predictions" => $predictions
+);
+
+$ret_json = json_encode($ret_arr);
+
+header('Content-type: application/json');
+echo $ret_json;
 
 ?>
