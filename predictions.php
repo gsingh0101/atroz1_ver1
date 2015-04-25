e7b93cd29cb50b1848faf2705a594146e7929cfa
unknown <dell@dell-PC.(none)>
2015-04-25 15:21:40 +0530

    Added parameters for configuration

--- predictions.php
+++ predictions.php
@@ -1,5 +1,9 @@
 <?php
 $_DEBUG = false;
+$_DBINTEGRATE = false;
+$_LOGINFILE = false;
+$LOGFILENAME = "customerinputlog.txt";
+$CEXECUTABLENAME = "pred2.exe";
 
 $where_form_is="http://".$_SERVER['SERVER_NAME'].strrev(strstr(strrev($_SERVER['PHP_SELF']),"/"));
 
@@ -42,42 +46,46 @@ $pob = getParam('pob');
 //echo "<br/>";
 
 // INSERT IN DB
+if ($_DBINTEGRATE)
+{
 include("config.php");
-$conn = mysql_connect($db_host, $db_user, $db_pass);
-if(!$conn) die ('Could not connect to database: '.mysql_error());
-mysql_select_db($db_name, $conn);
+	$conn = mysql_connect($db_host, $db_user, $db_pass);
+	if(!$conn) die ('Could not connect to database: '.mysql_error());
+	mysql_select_db($db_name, $conn);
 
-$query = "INSERT into `".$db_table."` (dob, mob, yob, hob,minob,city,firstname,middlename,lastname,country) VALUES ('" . $dob . "','" . $mob . "','" . $yob . "','" . $hob . "','" . $minob . "','" . $pob . "','" . $firstname . "','" . $middlename . "','" . $lastname . "','" . $country . "')";
+	$query = "INSERT into `".$db_table."` (dob, mob, yob, hob,minob,city,firstname,middlename,lastname,country) VALUES ('" . $dob . "','" . $mob . "','" . $yob . "','" . $hob . "','" . $minob . "','" . $pob . "','" . $firstname . "','" . $middlename . "','" . $lastname . "','" . $country . "')";
 
-if ($_DEBUG)
-	echo $query . "<br/>";
-$mysql_err = mysql_query($query);
-if ($_DEBUG)
-	if (!$mysql_err)
-		echo mysql_error();
+	if ($_DEBUG)
+		echo $query . "<br/>";
+	$mysql_err = mysql_query($query);
+	if ($_DEBUG)
+		if (!$mysql_err)
+			echo mysql_error();
 
-mysql_close($conn);
-
+	mysql_close($conn);
+}
 
 // LOG THE DETAILS
-$fname = "customerinputlog.txt";
-$fh = fopen($fname, 'a') or die ("Can't open log file");
-fwrite($fh, "
-Prediction performed at: " . date("Y-m-d H:i:s") . "
-Date of Birth: " . $dob . " 
-Country List: " . $country . " 
-First Name: " . $firstname . " 
-Middle Name: " . $middlename . " 
-Surname: " . $lastname . " 
-Place of Birth: " . $pob . " 
-Time of Birth HOUR:MIN: " . $hob . ":" . $minob . " 
+if ($_LOGINFILE)
+{
+	$fname = $LOGFILENAME;
+	$fh = fopen($fname, 'a') or die ("Can't open log file");
+	fwrite($fh, "
+	Prediction performed at: " . date("Y-m-d H:i:s") . "
+	Date of Birth: " . $dob . " 
+	Country List: " . $country . " 
+	First Name: " . $firstname . " 
+	Middle Name: " . $middlename . " 
+	Surname: " . $lastname . " 
+	Place of Birth: " . $pob . " 
+	Time of Birth HOUR:MIN: " . $hob . ":" . $minob . " 
 
-");
-
+	");
+}
 
 // CALL EXEC
 // change arguments here ---
-$predict_cmd = "pred2.exe" . " " . $dob . " " . $mob . " " . $yob . " " . $hob . " " . $minob . " " . $pob;
+$predict_cmd = $CEXECUTABLENAME . " " . $dob . " " . $mob . " " . $yob . " " . $hob . " " . $minob . " " . $pob;
 $out_arr = Array();
 $ret_arr = Array();
 //$exec_ret = exec("date /T", $out_arr, $ret_arr);
